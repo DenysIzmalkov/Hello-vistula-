c46ff609e2f59e32351d082f6eab59799ccad32e
DenysIzmalkov <133084313+DenysIzmalkov@users.noreply.github.com>
2023-10-19 15:07:05 +0300

    Add files via upload

--- hellovistulaa/src/Main.java
+++ hellovistulaa/src/Main.java
@@ -1,5 +1,9 @@
-class Simple{
-    public static void main(String args[]){
+class Simple {
+    public static void main(String args[]) {
         System.out.println("Hello Vistula");
+        System.out.println("This is a debug example.");
+        int x = 10;
+        int y = 20;
+        System.out.println("The sum of x and y is: " + (x + y));
     }
-}  
+}SSSSS
